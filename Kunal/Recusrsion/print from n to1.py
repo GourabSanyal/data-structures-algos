a6a719cb23ff1c32e98f5a8e03a69bd4d9a8aa8c
sanyal.gourab@gmali.com <sanyal.gourab@gmail.com>
2021-11-13 19:27:33 +0530

    practice

---
+++ Kunal/Recusrsion/print from n to1.py
@@ -0,0 +1,14 @@
+# 5 -> 4 -> 3 -> 2-> 1
+
+
+def func(n):
+    if n == 1:
+        return 1
+    while (n >= 1):
+        print(n, end=' ')
+        n = n - 1
+        return func(n)
+
+
+n = 5
+print(func(n))
